e2bcf9a28e0437fadb87cb6a17411b13cbe6caee
ningbidmini <67812699+ningbidmini@users.noreply.github.com>
2022-02-22 21:06:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,26 @@ require 'vendor/autoload.php';
 
 use \Firebase\JWT\JWT;
 
-$token_payload = [
-  'iss' => 'https://github.com/auth0/php-jwt-example',
-  'sub' => '123456',
-  'name' => 'John Doe',
-  'email' => 'info@auth0.com'
-];
+if(isset($_POST['dataset'])){ $dataset = $_POST['dataset']; }else{
+  $dataset=array(
+    'url'=>'https://www.dru.ac.th/',
+    'email'=>'rapin.t@dru.ac.th',
+    'fullname'=>'Raping Tonprasert',
+  );
+}
+if(isset($_POST['token'])){ $token = $_POST['token']; }else{  $token=""; }
+
+
+// $token_payload = [
+//   'iss' => 'https://github.com/auth0/php-jwt-example',
+//   'sub' => '123456',
+//   'name' => 'John Doe',
+//   'email' => 'info@auth0.com'
+// ];
+$token_payload = $dataset;
 
 // This is your client secret
-$key = '__test_secret__';
+$key = 'tossapol.c@dru.ac.th';
 
 // This is your id token
 $jwt = JWT::encode($token_payload, base64_decode(strtr($key, '-_', '+/')), 'HS256');
